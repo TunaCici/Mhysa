4c509f0951b204121e5041b4bc08b9e2a3218db0
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-01-10 21:11:49 +0300

    Added binary_add.

---
+++ src/Formatting/NumberFormatting.cpp
@@ -0,0 +1,77 @@
+#include <string>
+
+#include "Spdlog/spdlog.h"
+#include "Formatting/NumberFormatting.hpp"
+
+namespace formatting {
+
+bool is_binary(const std::string &arr) {
+    bool ret_value = false;
+
+    int i = 0;
+    for (i = 0; i < arr.size(); i++) {
+        if (arr[i] != '0' && arr[i] != '1') {
+            break;
+        }
+    }
+
+    if (i == arr.size()) {
+        ret_value = true;
+    }
+
+    return ret_value;
+}
+
+void binary_add(std::string& target, const std::string& arr1, const std::string& arr2) {
+        const int arr1_size = arr1.size();
+        const int arr2_size = arr2.size();
+
+        char sum = '0';
+        char carry = '0';
+        char extra_carry = '0';    
+
+        if (!is_binary(arr1) || !is_binary(arr2)) {
+            /* TODO: Arrays are not valid. Throw exception? */
+            return;
+        }
+
+        if (arr1_size != arr2_size) {
+            /* TODO: Not the same size. Throw exception? */
+            return;
+        }
+        
+        /* Prepare target string */
+        target.assign(arr1_size + 1, '0');
+
+        for (int i = arr1_size - 1; 0 <= i; i--) {
+            sum = '0';
+            extra_carry = '0';
+
+            /* 1. Add two bits normally */
+            if (arr1[i] != arr2[i]) {
+                sum = '1';
+            }
+            else if (arr1[i] == '1') {
+                sum = '0';
+                extra_carry = '1';
+            }
+
+            /* 2. Add carry */
+            if (carry != sum) {
+                sum = '1';
+            }
+            else if (carry == '1') {
+                sum = '0';
+                extra_carry = '1';
+            }
+
+            /* Target size is (n+1) */
+            target[i + 1] = sum;
+            carry = extra_carry;
+        }
+        
+        /* Don't forget the leftmost bit */
+        target[0] = carry; 
+    }
+
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <string>
 
 #include "Spdlog/spdlog.h"
 #include "Algorithms/Sorting.hpp"
+#include "Formatting/NumberFormatting.hpp"
 
 int main(int argc, char** argv) {
     int n_result = -1;
@@ -11,23 +13,16 @@ int main(int argc, char** argv) {
     // Set debug level
     spdlog::set_level(spdlog::level::debug);
 
-    // Generate random ints
-    std::random_device                  rand_dev;
-    std::mt19937                        generator(rand_dev());
-    std::uniform_int_distribution<int> dist(1, 50);
+    const std::string arr1 = "10";
+    const std::string arr2 = "11";
 
-    std::vector<int> my_arr;
-    for (auto i = 0; i < 16; i++) {
-        my_arr.push_back(dist(generator));
-    }
+    std::string result;
+    
+    formatting::binary_add(result, arr1, arr2);
 
-    sorting::insertion_sort(my_arr, false);
-
-    for (auto i = 0; i < my_arr.size(); i++) {
-        std::cout << my_arr[i] << ' ';
-    }
-
-    std::cout << std::endl;
+    spdlog::info("arr1:\t\t{}", arr1);
+    spdlog::info("arr2:\t\t{}", arr2);
+    spdlog::info("result:\t{}", result);
 
     return n_result;
-} 
+}  
