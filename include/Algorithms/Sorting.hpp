89f4474453c69baf4999a5d4bef4add7f46ca7d2
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-02-04 21:04:16 +0300

    Added the 'bubble_sort' algorithm.

--- include/Algorithms/Sorting.hpp
+++ include/Algorithms/Sorting.hpp
@@ -7,9 +7,10 @@ namespace sorting {
 
 /* Utilities */
 bool in_bound(const int& target, const int& left, const int& right);
-bool is_ordered(std::vector<int>& arr, bool ascending = true);
+bool is_ordered(const std::vector<int>& arr, bool ascending = true);
 
 /* Sorting functions */
+void bubble_sort(std::vector<int>& unsorted_arr, bool ascending = true);
 void insertion_sort(std::vector<int>& unsorted_arr, bool ascending = true);
 
 }
--- src/Algorithms/Sorting.cpp
+++ src/Algorithms/Sorting.cpp
@@ -6,7 +6,7 @@ bool in_bound(const int& target, const int& left, const int& right) {
     return left < target && target < right;
 }
 
-bool is_ordered(std::vector<int>& arr, bool ascending) {
+bool is_ordered(const std::vector<int>& arr, const bool ascending) {
     bool ret_value = false;
 
     if (arr.size() <= 1) {
@@ -37,30 +37,49 @@ bool is_ordered(std::vector<int>& arr, bool ascending) {
 void insertion_sort(std::vector<int>& unsorted_arr, bool ascending) {
     if (unsorted_arr.size() <= 1) {
         /* No need to sort */
+        return;
     }
-    else {
-        /* Control values */
-        int start_idx = (ascending == true ? 1 : unsorted_arr.size() - 1);
-        int move_by_one = (ascending == true ? 1 : - 1);
-        int arr_size = unsorted_arr.size();
 
-        /*
-        * Depending on the sorting order start either from left or right
-        * Depending on the sorting order move_by_one to either right or left
-        */
-        for (int i = start_idx; in_bound(i, -1, arr_size); i+=move_by_one) {
-            int on_hand = unsorted_arr[i];
+    /* Control values */
+    int start_idx = (ascending == true ? 1 : unsorted_arr.size() - 1);
+    int move_by_one = (ascending == true ? 1 : - 1);
+    const int arr_size = unsorted_arr.size();
 
-            int j = i - move_by_one;
-            while (in_bound(j, -1, arr_size) && on_hand < unsorted_arr[j]) {
-                unsorted_arr[j + move_by_one] = unsorted_arr[j];
-                j -= move_by_one;
-            }
-            unsorted_arr[j + move_by_one] = on_hand;
-        }        
+    /*
+    * Depending on the sorting order start either from left or right
+    * Depending on the sorting order move_by_one to either right or left
+    */
+    for (int i = start_idx; in_bound(i, -1, arr_size); i+=move_by_one) {
+        int on_hand = unsorted_arr[i];
+
+        int j = i - move_by_one;
+        while (in_bound(j, -1, arr_size) && on_hand < unsorted_arr[j]) {
+            unsorted_arr[j + move_by_one] = unsorted_arr[j];
+            j -= move_by_one;
+        }
+        unsorted_arr[j + move_by_one] = on_hand;
     }
 }
 
+void bubble_sort(std::vector<int>& unsorted_arr, bool ascending) {
+    if (unsorted_arr.size() <= 1) {
+        /* No need to sort */
+        return;
+    }
 
+    for (int i = unsorted_arr.size() - 1; 0 <= i; i--) {
+        for (int j = 1; j <= i; j++) {
+            bool left_bigger = unsorted_arr[j] < unsorted_arr[j - 1];
+            bool left_smaller = unsorted_arr[j - 1] < unsorted_arr[j];
+
+            if(ascending && left_bigger) {
+                std::swap(unsorted_arr[j], unsorted_arr[j - 1]);
+            }
+            else if (!ascending && left_smaller){
+                std::swap(unsorted_arr[j], unsorted_arr[j - 1]);
+            }
+        }
+    }
+}
 
 }
