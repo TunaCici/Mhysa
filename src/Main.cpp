a00cdb7836d290df89a1c7f2b9e181ee6fddaddb
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-05-08 20:30:34 +0300

    Added a new 'Experimental' code: RodCutting.

--- src/Main.cpp
+++ src/Main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char** argv) {
     }
 
     /* Defined in src/Experimental/CoinChange.cpp */
-    unsigned int coin_change(const std::vector<unsigned short> &banknotes, const int &target);
+    extern unsigned int coin_change(const std::vector<unsigned short> &banknotes, const int &target);
 
     const std::vector<unsigned short> banknotes = {1u, 2u, 5u, 10u, 20u, 50u, 100u};
 
@@ -44,6 +44,15 @@ int main(int argc, char** argv) {
         DLOG(INFO) << "coin_change(" << i * 10u << ") is " << coin_change(banknotes, i * 10u);
     }
 
+    /* Defined in src/Experimental/RodCutting.cpp */
+    extern unsigned int cut_rod(const std::vector<unsigned int> &prices, const int &n);
+
+    const std::vector<unsigned int> prices = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+
+    for (auto i = 0; i < 11; i++)
+    {
+        DLOG(INFO) << "cut_rod(" << i << ") is " << cut_rod(prices, i);
+    }
 #endif
 
     nResult = EXIT_SUCCESS;
