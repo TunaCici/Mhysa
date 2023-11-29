af56cb63742882ba9bd876fa6be7c01ed15663e7
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-11-29 22:04:46 +0300

    experimental: Sometimes I feel like I'm the dumbest person out alive. How did I even gradute? Let's look at this later... i hate myself
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

---
+++ src/Experimental/ZigZagConversion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+        string convert(string s, int numRows) {
+                std::string retValue{}; 
+
+
+
+                return retValue;
+        }
+};
+
+int main ()
+{
+
+        Solution solution;
+
+        std::string testThis = "PAYPALISHIRING";
+        solution.convert(testThis, 1);
+
+        return 0;
+}
