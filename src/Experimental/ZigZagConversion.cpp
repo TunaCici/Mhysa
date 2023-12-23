1210bf06b8376753758457023ba6769390061e2b
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-12-23 17:15:21 +0300

    experimental: I was not as bad as I made myself out to be.
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

--- src/Experimental/ZigZagConversion.cpp
+++ src/Experimental/ZigZagConversion.cpp
@@ -5,9 +5,63 @@ using namespace std;
 class Solution {
 public:
         string convert(string s, int numRows) {
-                std::string retValue{}; 
+                std::string retValue{};
 
+                /* Buffer (TODO: make it dynamic the next time) */
+                char buff[1024][1024] = {0};
 
+                bool isGoingUp = false;
+
+                int targetX = 0;
+                int targetY = 0;
+
+                /* edge case when numRows is 1 */
+                if (numRows == 1) {
+                        return s;
+                }
+
+                /* print it onto the 2D buffer */
+                for (auto i = 0; i < s.length(); i++) {
+                        buff[targetY][targetX] = s[i];
+                        std::cout << "put " << s[i] << " @ " << targetY << ", " << targetX << std::endl;
+
+                        /* should reflect? */
+                        if (!isGoingUp && numRows <= (targetY + 1)) {
+                                isGoingUp = true;
+                        } else if (isGoingUp && (targetY - 1) < 0) {
+                                isGoingUp = false;
+                        }
+
+                        /* determine next target idx */ 
+                        if (isGoingUp) {
+                                targetY--;
+                                targetX++;
+                        } else {
+                                targetY++;
+                        }
+                }
+
+                /* debug */
+                for (auto i = 0; i < numRows; i++) {
+                        for (auto j = 0; j <= targetX; j++) {
+                                if (buff[i][j] != 0) {
+                                        std::cout << buff[i][j];
+                                } else {
+                                        std::cout << ' ';
+                                }
+                        }
+
+                        std::cout << std::endl;
+                }
+
+                /* finally, convert to 1D array */
+                for (auto i = 0; i < numRows; i++) {
+                        for (auto j = 0; j <= targetX; j++) {
+                                if (buff[i][j] != 0) {
+                                        retValue.push_back(buff[i][j]);
+                                }
+                        }
+                }
 
                 return retValue;
         }
@@ -15,11 +69,12 @@ public:
 
 int main ()
 {
-
         Solution solution;
 
-        std::string testThis = "PAYPALISHIRING";
-        solution.convert(testThis, 1);
+        std::string testThis = "SAYGOODBYE";
+        std::string result = solution.convert(testThis, 3);
+
+        std::cout << result << std::endl;
 
         return 0;
 }
