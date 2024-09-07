ef484bdb8542eeab2ea7c8bae27d72ac5832d18c
TunaCici <36923239+TunaCici@users.noreply.github.com>
2024-09-07 23:03:53 +0300

    sel4: tutorial: Capabilities is done.

--- seL4/capabilities/src/main.c
+++ seL4/capabilities/src/main.c
@@ -11,11 +11,19 @@ int main(int argc, char *argv[]) {
     the environment variables set up by the default crt0.S */
     seL4_BootInfo *info = platsupport_get_bootinfo();
 
+    /*
+     * Amount of slots in a CNode is stored in info->CNodeSizeBits
+     */
     size_t initial_cnode_object_size = BIT(info->initThreadCNodeSizeBits);
     printf("Initial CNode is %zu slots in size\n", initial_cnode_object_size);
 
-
-    size_t initial_cnode_object_size_bytes = 0; // TODO calculate this.
+    /*
+     * Each slot is 2^(seL4_SlotBits) bytes in size
+     *      16 bytes in 32-bit systems
+     *      32 bytes in 64-bit systems
+     */
+    size_t initial_cnode_object_size_bytes =
+        BIT(info->initThreadCNodeSizeBits + seL4_SlotBits);
     printf("The CNode is %zu bytes in size\n", initial_cnode_object_size_bytes);
 
     seL4_CPtr first_free_slot = info->empty.start;
@@ -24,13 +32,23 @@ int main(int argc, char *argv[]) {
                                        seL4_AllRights);
     ZF_LOGF_IF(error, "Failed to copy cap!");
     seL4_CPtr last_slot = info->empty.end - 1;
-    /* TODO use seL4_CNode_Copy to make another copy of the initial TCB capability to the last slot in the CSpace */
+
+    /*
+     * I kinda like this idea of CSlots
+     */
+    error = seL4_CNode_Copy(seL4_CapInitThreadCNode, last_slot, seL4_WordBits,
+                            seL4_CapInitThreadCNode, first_free_slot, seL4_WordBits,
+                            seL4_AllRights);
+    ZF_LOGF_IF(error, "Failed to copy cap to the last slot!");
 
     /* set the priority of the root task */
     error = seL4_TCB_SetPriority(last_slot, last_slot, 10);
     ZF_LOGF_IF(error, "Failed to set priority");
 
-    // TODO delete the created TCB capabilities
+    /*
+     * Simply delete all child capabilities (first_free_slot & last_slot)
+     */
+    error = seL4_CNode_Revoke(seL4_CapInitThreadCNode, seL4_CapInitThreadTCB, seL4_WordBits);
 
     // check first_free_slot is empty
     error = seL4_CNode_Move(seL4_CapInitThreadCNode, first_free_slot, seL4_WordBits,
@@ -43,7 +61,12 @@ int main(int argc, char *argv[]) {
     ZF_LOGF_IF(error != seL4_FailedLookup, "last_slot is not empty");
 
     printf("Suspending current thread\n");
-    // TODO suspend the current thread
+
+    /*
+     * This is interesting because I didn't pass the actual capability,
+     * but rather its slot index in the CSpace root. Implicit invocation?
+     */
+    error = seL4_TCB_Suspend(seL4_CapInitThreadTCB);
     ZF_LOGF("Failed to suspend current thread\n");
 
     return 0;
