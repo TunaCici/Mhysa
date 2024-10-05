a9a766b3efd46e66c0faf3f35face0c4cf1dfd67
Tuna Cici <tunacici7@gmail.com>
2024-10-05 16:13:47 +0300

    sel4: tutorial: Faults is done.
    Signed-off-by: Tuna Cici <tunacici7@gmail.com>

--- seL4/fault-handlers/handler.c
+++ seL4/fault-handlers/handler.c
@@ -48,7 +48,7 @@ int main(void)
         badged_faulter_fault_ep_cap,
         seL4_WordBits,
         handler_cspace_root,
-        /* EXERCISE: Which capability should be the source argument here? */-1,
+        faulter_fault_ep_cap, /* we are badging to this cap */
         seL4_WordBits,
         seL4_AllRights, FAULTER_BADGE_VALUE);
 
@@ -63,7 +63,7 @@ int main(void)
 
     error = seL4_CNode_Copy(
         faulter_cspace_root,
-        /* EXERCISE: What should the destination be for this cap copy? */-1,
+        foreign_badged_faulter_empty_slot_cap, /* target slot on faulter cspace */
         seL4_WordBits,
         handler_cspace_root,
         badged_faulter_fault_ep_cap,
@@ -75,22 +75,65 @@ int main(void)
            "faulter's cspace.\n"
            PROGNAME "(Only necessary on Master kernel.)\n");
 
+    /*
+     * This is an interesting detail about the design
+     *
+     * seL4_TCB_SetSpace is used to modify CSpace, VSpace and fault ep of a TCB
+     * meaning, the faulter needs write/modify access to target TCB
+     *
+     * it's not crazy, but still suprised me to learn this
+     * so yeah, faulter has WRITE/MODIFY access to target TCB
+     */
+    error = seL4_TCB_SetSpace(
+        faulter_tcb_cap,
+        foreign_badged_faulter_empty_slot_cap,
+        faulter_cspace_root,
+        0,
+        faulter_vspace_root,
+        0);
 
+    ZF_LOGF_IF(error != 0, PROGNAME "Failed seL4_TCB_SetSpace on faulter TCB");
+    printf(PROGNAME "Successfull seL4_TCB_SetSpace on faulter TCB\n");
 
+    /*
+     * reply back to the faulter
+     */
+    seL4_Reply(seL4_MessageInfo_new(0, 0, 0, 0));
 
+    /*
+     * now we wait on the endpoint for any faults
+     */
+    
+    seL4_Word badge = 0;
+    seL4_MessageInfo_t msginfo = {0};
 
+    while (1) {
+        msginfo = seL4_Recv(foreign_badged_faulter_empty_slot_cap, &badge);
+        printf(PROGNAME "Received fault from %lu\n", badge);
 
+        /*
+         * figure out what was the fault about (refere to manual here)
+         * the tutorial tells us that only CapFault will be generated
+         * so, i just get the IPC msgs of a CapFault, instead of a switch-case
+         */
+        foreign_faulter_capfault_cap = seL4_GetMR(seL4_CapFault_Addr);
 
+        /*
+         * ready to handle it
+         * for now all we do is put a valid capability to the faulting cap slot
+         */
+        error = seL4_CNode_Copy(
+            faulter_cspace_root,
+            foreign_faulter_capfault_cap,
+            seL4_WordBits,
+            handler_cspace_root,
+            sequencing_ep_cap, /* randomly choosen */
+            seL4_WordBits,
+            seL4_AllRights);
+        ZF_LOGF_IF(error != 0, PROGNAME "Failed handle CapFault\n");
 
-
-
-
-
-
-
-
-
-
+        seL4_Reply(seL4_MessageInfo_new(0, 0, 0, 0));
+    }
 
     return 0;
 }
