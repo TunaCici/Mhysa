ef128e77f0a49e4ca26057b870ad41f926ce696c
Tuna Cici <tunacici7@gmail.com>
2024-11-11 21:03:36 +0300

    tools: ncurses: Hello world.
    Signed-off-by: Tuna Cici <tunacici7@gmail.com>

--- Tools/ncurses.c
+++ Tools/ncurses.c
@@ -1,2 +1,16 @@
-# Follow the below guide
-# https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/index.html
+// Follow the below guide
+// https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/index.html
+
+#include <ncurses.h>
+
+int main()
+{	
+	initscr();			/* Start curses mode 		  */
+	printw("Hello World !!!");	/* Print Hello World		  */
+	refresh();			/* Print it on to the real screen */
+	getch();			/* Wait for user input */
+	endwin();			/* End curses mode		  */
+
+	return 0;
+}
+
